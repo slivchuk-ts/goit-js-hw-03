93a856145584c8ef7b26c4fce4d6e63fecd833c2
slivchuk-ts <taniaslivchuk97@gmail.com>
2024-06-02 20:09:15 +0200

    1

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+function slugify(title) {
+   const slugLower = title.toLowerCase()
+    const slugArray = slugLower.split(" ")
+    const slugSlash = slugArray.join("-")
+   
+    return slugSlash;
+   }
+
+console.log("-----task1-----");
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log("");
